3205ab22a8d98854fd1402a570800c023ed0b908
Sachin Mour <rtr.sachinmour@gmail.com>
2016-06-21 08:12:24 +0100

    Updated Contact

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-class Footer extends React.Component{
-  
-  render() {
-    return (
-      <div id="contact">
-        <p><a href="https://github.com/sachinmour/book-trading-app/">@sachinmour</a></p>
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <div id="contact">
+        <p><a href="https://github.com/sachinmour/pinterest-clone">@sachinmour</a></p>
       </div>
-    );
-  }
-  
+        );
+    }
+
 }
 
 export default Footer;
